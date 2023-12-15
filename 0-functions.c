9f63375046c81fe577bc1db463462fdfc6f17db5
Manuel1426 <immaosas@gmail.com>
2023-12-14 16:05:30 -0800

    Stacks, Queues - LIFO, FIFO

--- 0-functions.c
+++ 0-functions.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
 * execute - executes the opcode
 * @stack: head linked list - stack
@@ -50,3 +51,45 @@ int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 		exit(EXIT_FAILURE); }
 	return (1);
 }
+
+/**
+ * addnode - add node to the head stack
+ * @head: head of the stack
+ * @n: new_value
+ * Return: no return
+*/
+void addnode(stack_t **head, int n)
+{
+
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (aux)
+		aux->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
+
+/**
+* free_stack - frees a doubly linked list
+* @head: head of the stack
+*/
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
+
--- 5-nop.c
+++ 5-nop.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
-  *f_nop- nothing
+  *f_nop- does nothing
   *@head: stack head
   *@counter: line_number
   *Return: no return
